611d317ca31dbf51301e659b428b8c0728cbeab0
Gogs <gogs@fake.local>
2020-11-10 14:07:51 +0200

    3

--- scripts.js
+++ scripts.js
@@ -25,15 +25,34 @@
 //     }
 //     document.write(str);
 // }
-let but=document.getElementById("res");
-let result=document.getElementById("result");
-but.addEventListener("click",Min);
-function Min()
+// let but=document.getElementById("res");
+// let result=document.getElementById("result");
+// but.addEventListener("click",Min);
+// function Min()
+// {
+//     let a=parseInt(document.getElementById("n1").value);
+//     let b=parseInt(document.getElementById("n2").value);
+//     if(a>b) result.innerText=b.toString();
+//     else result.innerText=a.toString();
+// }
+for (var i=1;i<=8;i++)
 {
-    let a=parseInt(document.getElementById("n1").value);
-    let b=parseInt(document.getElementById("n2").value);
-    if(a>b) result.innerText=b.toString();
-    else result.innerText=a.toString();
+    let main = document.getElementById("main");
+    var elem = document.createElement("div");
+
+    for (var j=1;j<=8;j++) {
+
+        var ch = document.createElement("div");
+        if((i+j)%2==0) {
+            elem.className = "black";
+        }
+        else {
+            elem.className="white";
+        }
+        elem.appendChild(ch);
+    }
 }
+
+
 
 
